df0543ba0174bab62877552f1441b05f4f76f897
YogaJi <y.disturbia@gmail.com>
2022-02-21 16:53:19 +0800

    add four TestMethod for calculation
    add four tests structures for calculations and each test method has four DataRow

--- ASP.Net Core Web API/Controllers/CalculateController.cs
+++ ASP.Net Core Web API/Controllers/CalculateController.cs
@@ -8,33 +8,39 @@ using System.Threading.Tasks;
 
 namespace ASP.Net_Core_Web_API.Controllers
 {
+    //set each calculate method as api/calculator/'method'
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class CalculateController : ControllerBase
     {
-  
+        //get the input from Library
+        //set the numbers are double,
+        //either negative or positive number is allowed
         [HttpGet]
-        public double? Add(double valueA, double valueB)
+        public double? Add(double? valueA, double? valueB)
         {
+            //add
             return valueA + valueB;
         }
 
         [HttpGet]
-        public double? Subtract(double valueA, double valueB)
+        public double Subtract(double valueA, double valueB)
         {
+            //subtract
             return valueA - valueB;
         }
 
         [HttpGet]
         public double? Multiple(double valueA, double valueB)
         {
+            //mutiple
             return valueA * valueB;
         }
 
         [HttpGet]
-        public double? Divise(double valueA, double valueB)
+        public double? Divide(double valueA, double valueB)
         {
-            //situation:  number / 0
+            //situation:  number / 0 and response 400
             if (valueB == 0) {
                 Response.StatusCode = 400;
                 Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Bad Request";
@@ -42,13 +48,10 @@ namespace ASP.Net_Core_Web_API.Controllers
             }
             else
             {
+                //divise
                 return valueA / valueB;
             }
         }
     }
- /*   public class Input
-    {
-        public int valueA { get; set; }
-        public int valueB { get; set; }
-    }*/
+
 }
--- Test Project/CalculatorTest.cs
+++ Test Project/CalculatorTest.cs
@@ -1,13 +1,59 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASP.Net_Core_Web_API;
+using ASP.Net_Core_Web_API.Controllers;
 
 namespace Test_Project
 {
     [TestClass]
-    public class calculatorTest
+    public class CalculatorTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly CalculateController _CalculateController;
+
+        public CalculatorTest()
         {
+            _CalculateController = new CalculateController();
+        }
+
+
+        [TestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(-1, -3, -4)]
+        [DataRow(0.01, 0.6600, 0.67)]
+        [DataRow(-0.05, -0.05, -0.1)]
+        public void Test_Add_Input_Int_Double_Negative_Postive(double valueA, double valueB, double expected)
+        {
+            
+            Assert.AreEqual(expected, _CalculateController.Add(valueA, valueB));
+        }
+
+       [TestMethod]
+        [DataRow(1, 2, -1)]
+        [DataRow(-1, -3, 2)]
+        [DataRow(1, 5, -4)]
+        [DataRow(-1, 1, -2)]
+        public void Test_Subtract_Input_Int_Double_Negative_Postive(double valueA, double valueB, double expected)
+        {
+            Assert.AreEqual(expected, _CalculateController.Subtract(valueA, valueB));
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 2)]
+        [DataRow(-1, -3, 3)]
+        [DataRow(0.1, 0.6, 0.06)]
+        [DataRow(-5, 5, -25)]
+        public void Test_Multiple_Input_Int_Double_Negative_Postive(double valueA, double valueB, double expected)
+        {
+            Assert.AreEqual(expected, _CalculateController.Multiple(valueA, valueB));
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 0.5)]
+        [DataRow(-1, -1, 1)]
+        [DataRow(0.1, 2, 0.05)]
+        [DataRow(-5, -0.5, 10)]
+        public void Test_Divide_Input_Int_Double_Negative_Postive(double valueA, double valueB, double expected)
+        {
+            Assert.AreEqual(expected, _CalculateController.Divide(valueA, valueB));
         }
     }
 }
