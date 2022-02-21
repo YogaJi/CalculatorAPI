21baa788ff908a670f8e71d20e446ee9b7bbd613
YogaJi <y.disturbia@gmail.com>
2022-02-21 15:52:32 +0800

    add library structure & API functions
    1.set up the library structure with basic form and use get method.
    2.set up the calculation function with HttpGet method 400 response is added for the situation num / 0
    3. add comments

---
+++ ASP.Net Core Web API/Controllers/CalculateController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Core_Web_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+  
+        [HttpGet]
+        public double? Add(double valueA, double valueB)
+        {
+            return valueA + valueB;
+        }
+
+        [HttpGet]
+        public double? Subtract(double valueA, double valueB)
+        {
+            return valueA - valueB;
+        }
+
+        [HttpGet]
+        public double? Multiple(double valueA, double valueB)
+        {
+            return valueA * valueB;
+        }
+
+        [HttpGet]
+        public double? Divise(double valueA, double valueB)
+        {
+            //situation:  number / 0
+            if (valueB == 0) {
+                Response.StatusCode = 400;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Bad Request";
+                return null;
+            }
+            else
+            {
+                return valueA / valueB;
+            }
+        }
+    }
+ /*   public class Input
+    {
+        public int valueA { get; set; }
+        public int valueB { get; set; }
+    }*/
+}
