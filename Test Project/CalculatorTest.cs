44eca8efa417812ee1ec2a70a5c39dadc24047ec
YogaJi <y.disturbia@gmail.com>
2022-02-21 16:10:55 +0800

    rename test file & set up test
    add the API project reference in the test project and set up the connection in the CalculatorTest.cs

--- Test Project/CalculatorTest.cs
+++ Test Project/CalculatorTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Test_Project
 {
     [TestClass]
-    public class UnitTest1
+    public class calculatorTest
     {
         [TestMethod]
         public void TestMethod1()
