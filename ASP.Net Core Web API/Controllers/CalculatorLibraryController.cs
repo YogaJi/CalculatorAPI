2939462a3086da2353d0f680b614f071ab48ffa1
YogaJi <y.disturbia@gmail.com>
2022-02-22 10:47:12 +0800

    add CalculatorLibrary terminal
    Add calculator library,
    new API for calculator library,
    set up terminal,
    add new tests,
    add comments

---
+++ ASP.Net Core Web API/Controllers/CalculatorLibraryController.cs
@@ -0,0 +1,57 @@
+﻿using CalculatorLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Core_Web_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorLibraryController : ControllerBase
+    {
+        private readonly Calculator _calculator;
+        public CalculatorLibraryController()
+        {
+            _calculator = new Calculator();
+        }
+
+        [HttpGet]
+        public double Add(double valueA, double valueB)
+        {
+            return _calculator.Add(valueA, valueB);
+        }
+
+        [HttpGet]
+        public double Subtract(double valueA, double valueB)
+        {
+            return _calculator.Subtract(valueA, valueB);
+        }
+
+        [HttpGet]
+        public double Multiply(double valueA, double valueB)
+        {
+            return _calculator.Multiple(valueA, valueB);
+        }
+        [HttpGet]
+        public double? Divide(double valueA, double valueB)
+        {
+            //situation:  number / 0 and response 400
+            if (valueB == 0)
+            {
+                Response.StatusCode = 400;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Bad Request";
+                return null;
+            }
+            else
+            {
+                return valueA / valueB;
+            }
+
+        }
+
+    }
+}
---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        
+        static void Main(string[] args)
+        {
+            double valueA;
+            double valueB;
+            string userInput;
+            Console.WriteLine("Welcome to the Calculator:");
+            Console.WriteLine("Please input a valueA number");
+            userInput = Console.ReadLine();
+            valueA = Convert.ToDouble(userInput);
+
+            Console.WriteLine("Please enter a valueB number");
+            userInput = Console.ReadLine();
+            valueB = Convert.ToDouble(userInput);
+
+            var calculate = new Calculator();
+            Console.WriteLine("Addition: " + valueA + " + " + valueB + " = " + calculate.Add(valueA, valueB));
+            Console.WriteLine("Subtraction: " + valueA + " - " + valueB + " = " + calculate.Subtract(valueA, valueB));     
+            Console.WriteLine("Multiplication: " + valueA + " * " + valueB + " = " + calculate.Multiple(valueA, valueB));
+            if(valueB == 0)
+            {
+                Console.WriteLine("Division Error");
+            }
+            else
+            {
+                Console.WriteLine("Division: " + valueA + " / " + valueB + " = " + calculate.Divide(valueA, valueB));
+            }
+    
+        }//end of static 
+        
+        //Methods:
+        public double Add(double valueA, double valueB)
+        {
+            return valueA + valueB;
+        }
+
+        public double Subtract(double valueA, double valueB)
+        {
+            return valueA - valueB;
+        }
+
+        public double Multiple(double valueA, double valueB)
+        {
+            return valueA * valueB;
+        }
+
+        public double? Divide(double valueA, double valueB)
+        {
+            if(valueB == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                return valueA / valueB;
+            }
+  
+        }
+    }
+}
